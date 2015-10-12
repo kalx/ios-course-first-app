37d8a2ce83561359586a87c6b602ff66d627dbf1
Gian Michele Pileri <gmp@Gians-Mac-Pro.local>
2015-10-12 19:30:06 -0400

    Initial Commit

---
+++ MySuperCoolApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySuperCoolApp
+//
+//  Created by Gian Michele Pileri on 2015-10-12.
+//  Copyright (c) 2015 Gian Michele Pileri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
